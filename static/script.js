ed0549b7b06a782ac066dd9bbb40d57315080780
markcurtis1970 <mark.curtis@datastax.com>
2020-09-22 10:19:14 +0100

    Change for some formatting and DB values

--- static/script.js
+++ static/script.js
@@ -1,11 +1,14 @@
-// Our labels along the x-axis
-var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
-// For drawing the lines
-var africa = [86,114,106,106,107,111,133,221,783,2478];
-var asia = [282,350,411,502,635,809,947,1402,3700,5267];
-var europe = [168,170,178,190,203,276,408,547,675,734];
-var latinAmerica = [40,20,10,16,24,38,74,167,508,784];
-var northAmerica = [6,3,2,2,7,26,82,172,312,433];
+// colours
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
@@ -15,7 +18,10 @@ var myChart = new Chart(ctx, {
     datasets: [
       { 
         label: 'Temperature',
-        data: temps
+        data: temps,
+        backgroundColor: window.chartColors.blue,
+        borderColor: window.chartColors.blue,
+        fill: false
       }
     ]
   }
